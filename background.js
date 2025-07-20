7bdd8870d98d01de4eb2b6ebdb7f24a9f9b27e9c
mayuri311 <mmurugan@andrew.cmu.edu>
2025-07-20 19:11:23 -0400

    update background.js

--- background.js
+++ background.js
@@ -2,3 +2,14 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("PromptPilot installed.");
 });
+chrome.action.onClicked.addListener(async () => {
+  try {
+    await chrome.sidePanel.setOptions({
+      path: 'sidepanel.html',
+      enabled: true
+    });
+    await chrome.sidePanel.open({});
+  } catch (e) {
+    console.error('Failed to open side panel:', e);
+  }
+});
